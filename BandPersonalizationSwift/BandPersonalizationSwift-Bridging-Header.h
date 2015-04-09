b2758e81025261ea819213a2ffcc887c9107b788
Mark Thistle <mthistle@gmail.com>
2015-04-09 16:49:09 -0700

    Initial pass of port to Swift.

---
+++ BandPersonalizationSwift/BandPersonalizationSwift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BandPersonalizationSwift-Bridging-Header.h
+//  BandPersonalizationSwift
+//
+//  Created by Mark Thistle on 4/9/15.
+//  Copyright (c) 2015 New Thistle LLC. All rights reserved.
+//
+
+#ifndef BandPersonalizationSwift_BandPersonalizationSwift_Bridging_Header_h
+#define BandPersonalizationSwift_BandPersonalizationSwift_Bridging_Header_h
+
+#import <MicrosoftBandKit_iOS/MicrosoftBandKit_iOS.h>
+
+#endif
--- BandPersonalizationSwift/ViewController.swift
+++ BandPersonalizationSwift/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MSBClientManagerDelegate {
+    
+    @IBOutlet weak var txtOutput: UITextView!
+    @IBOutlet weak var accelLabel: UILabel!
+    weak var client: MSBClient?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MSBClientManager.sharedManager().delegate = self
+        if let client = MSBClientManager.sharedManager().attachedClients().first as? MSBClient {
+            self.client = client
+            MSBClientManager.sharedManager().connectClient(self.client)
+            self.output("Please wait. Connecting to Band...")
+        } else {
+            self.output("Failed! No Bands attached.")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func runExampleCode(sender: AnyObject) {
+        if let client = self.client {
+            if client.isDeviceConnected == false {
+                self.output("Band is not connected. Please wait....")
+                return
+            }
+            self.output("Updating MeTile image...")
+            var image = MSBImage(UIImage:UIImage(named: "SampleMeTileImage.jpg"))
+            client.personalizationManager.updateMeTileImage(image, completionHandler: { (error: NSError!) in
+                if error == nil {
+                    self.output("Successfully Finished!!!")
+                } else {
+                    self.output(error.localizedDescription)
+                }
+            })
+        } else {
+            self.output("Band is not connected. Please wait....")
+        }
+    }
+    
+    func output(message: String) {
+        self.txtOutput.text = NSString(format: "%@\n%@", self.txtOutput.text, message) as String
+        let p = self.txtOutput.contentOffset
+        self.txtOutput.setContentOffset(p, animated: false)
+        self.txtOutput.scrollRangeToVisible(NSMakeRange(self.txtOutput.text.lengthOfBytesUsingEncoding(NSASCIIStringEncoding), 0))
+    }
+    
+    // Mark - Client Manager Delegates
+    func clientManager(clientManager: MSBClientManager!, clientDidConnect client: MSBClient!) {
+        self.output("Band connected.")
+    }
+    
+    func clientManager(clientManager: MSBClientManager!, clientDidDisconnect client: MSBClient!) {
+        self.output(")Band disconnected.")
+    }
+    
+    func clientManager(clientManager: MSBClientManager!, client: MSBClient!, didFailToConnectWithError error: NSError!) {
+        self.output("Failed to connect to Band.")
+        self.output(error.description)
+    }
 }
+
 
